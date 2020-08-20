0fc0b28f1c9f93a66e5447c5e982cbfd46fce4c7
muhammadzulfa <12muhammadzulfa@gmail.com>
2020-08-20 21:59:36 +0700

    sada

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 # Create your views here.
 def index(request):
-    coba
     return render(request,'index.html',{'o':'<a href="#test">HELLO WORLD</a>'})
