2b3b4890e532c23c4da8f69b32379504b742de1b
muhammadzulfa <12muhammadzulfa@gmail.com>
2020-08-20 23:48:20 +0700

    sada

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,4 @@
 sudo docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
+docker tag hello $DOCKER_USERNAME/hello:latest
 sudo docker push muhammadzulfa/hello:latest
 docker-compose
